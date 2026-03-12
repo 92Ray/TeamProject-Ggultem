9f817d7ff60dd6213f0e6d132a825a88d54848be
KTH <sauce0428@gmail.com>
2026-03-12 18:05:41 +0900

    ChatMessages 객체 생성

--- GgultemProject/src/main/java/com/honey/domain/ChatMessages.java
+++ GgultemProject/src/main/java/com/honey/domain/ChatMessages.java
@@ -1,12 +1,42 @@
 package com.honey.domain;
 
-import java.util.Date;
+import com.honey.common.BaseTimeEntity;
 
-public class ChatMessages {
-	public int msgId;
-	public int roomId;
-	public int senderId;
-	public String content;
-	public int isRead;
-	public Date sendDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "chatmessages")
+@SequenceGenerator(name = "CHATMESSAGES_SEQ_GEN",
+		sequenceName = "CHATMESSAGES_SEQ",
+		initialValue = 1,
+		allocationSize = 1)
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessages extends BaseTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CHATMESSAGES_SEQ_GEN")
+	private Long msgId;
+	
+	@ManyToOne
+    @JoinColumn(name = "ROOM_ID") // 실제 DB 테이블의 FK 컬럼명을 지정
+	private ChatRoom chatRoom;
+	
+	private String senderId;
+	private String content;
+	private int isRead;
 }
