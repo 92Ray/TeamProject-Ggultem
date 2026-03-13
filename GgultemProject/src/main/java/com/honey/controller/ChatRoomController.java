1aff35bf033715c8482713f3dd0b363280365027
KTH <sauce0428@gmail.com>
2026-03-13 17:01:59 +0900

    ChatRoom 컨트롤러 CURD 생성 완료

--- GgultemProject/src/main/java/com/honey/controller/ChatRoomController.java
+++ GgultemProject/src/main/java/com/honey/controller/ChatRoomController.java
@@ -5,11 +5,15 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.honey.dto.ChatRoomDTO;
+import com.honey.dto.MemberDTO;
+import com.honey.dto.PageRequestDTO;
+import com.honey.dto.PageResponseDTO;
 import com.honey.service.ChatRoomService;
 
 import lombok.RequiredArgsConstructor;
@@ -32,6 +36,24 @@ public class ChatRoomController {
 	public Map<String, Long> register(@RequestBody ChatRoomDTO chatRoomDTO) {
 		Long no = service.register(chatRoomDTO);
 		return Map.of("NO", no);
+	}
+	
+	@GetMapping("/list")
+	public PageResponseDTO<ChatRoomDTO> list(PageRequestDTO pageRequestDTO) {
+		return service.list(pageRequestDTO);
+	}
+	
+	@PutMapping("/{roomId}")
+	public Map<String, String> modify(@PathVariable(name = "roomId") Long roomId, @RequestBody ChatRoomDTO chatRoomDTO) {
+		chatRoomDTO.setRoomId(roomId);
+		service.modify(chatRoomDTO);
+		return Map.of("RESULT", "SUCCESS");
+	}
+	
+	@PutMapping("/remove/{roomId}")
+	public Map<String, String> remove(@PathVariable(name = "roomId") Long roomId) {
+		service.remove(roomId);
+		return Map.of("RESULT", "SUCCESS");
 	}
 }
 	   
