c2066e222c8a10c23a0a41cbe87cb0a403844a09
Ray <dnl1992@naver.com>
2026-04-02 10:26:31 +0900

    [ 수정 ] 신고처리 재수정 및 read에서 목록으로 돌아가기 기능 추가

--- GgultemProject/src/main/java/com/honey/repository/ProcessedReportRepository.java
+++ GgultemProject/src/main/java/com/honey/repository/ProcessedReportRepository.java
@@ -1,8 +1,10 @@
 package com.honey.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.honey.domain.ProcessedReport;
 
 public interface ProcessedReportRepository extends JpaRepository<ProcessedReport, Long> {
-    // 기본적인 조치는 상속받은 메서드로 충분합니다.
+    Optional<ProcessedReport> findByReport_ReportId(Long reportId); // ✅ 추가
 }
--- GgultemProject/src/main/java/com/honey/service/ProcessedReportService.java
+++ GgultemProject/src/main/java/com/honey/service/ProcessedReportService.java
@@ -1,8 +1,13 @@
 package com.honey.service;
 
+import com.honey.dto.PageRequestDTO;
+import com.honey.dto.PageResponseDTO;
 import com.honey.dto.ProcessedReportDTO;
+import com.honey.dto.ReportDTO;
 
 public interface ProcessedReportService {
-	// 신고 처리 실행
-	Long process(ProcessedReportDTO dto);
+    Long process(ProcessedReportDTO dto);
+    PageResponseDTO<ReportDTO> list(PageRequestDTO pageRequestDTO);
+    ReportDTO getOne(Long reportId);
+    ProcessedReportDTO getOneProcessed(Long reportId);
 }
